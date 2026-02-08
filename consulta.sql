b3c03fe9885f72c1eac417441ba3d397c8d81440
Tayna Gaspar <taynagaspar75@gmail.com>
2026-02-08 17:12:22 -0300

    Add SQL queries for total revenue and best-selling products

--- consulta.sql
+++ consulta.sql
@@ -1,1 +1,16 @@
+-- Faturamento total do e-commerce
+
+SELECT
+    SUM(valor) AS faturamento_total
+FROM vendas;
+
+
+-- Produtos mais vendidos
+
+SELECT
+    produto,
+    SUM(valor) AS faturamento_total
+FROM vendas
+GROUP BY produto
+ORDER BY faturamento_total DESC;
 
